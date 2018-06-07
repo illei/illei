d1a3fafffb869c8a02931376906f1fe24eaede3e
illei <illei.adrian@yahoo.com>
2018-06-07 14:48:30 +0300

    reopen

--- big.one/js/modal.js
+++ big.one/js/modal.js
@@ -15,48 +15,8 @@ function close_modal() {
 
 }
 
-function verifica_user(sir)
-{
-	if(sir.match(user)){
-		return true;
-	
-	}
-	return false;
-}
-
-function verifica_pass(sir)
-{
-	if(sir.match(pass)){
-		return true;
-	
-	}
-	return false;
-		}
 
 
-function log_in(){
-	if(verifica_user(ref_user.value) && verifica_pass(ref_pass.value)){
-		
-		var ref_modal= document.getElementById('form');
-		var ref_modal2= document.getElementsByClassName("modal-succes");
-		ref_modal.style.display="none";
-		ref_modal2[0].style.display="block";
-
-	}
-
-	else{
-		alert("User sau Parola gresita");
-	}
-}
-
-function reveal(){
-	ref_pass.type="text";
-
-}
-
-function hide(){
-	ref_pass.type="password";
-}
 
 
 
